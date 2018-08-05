6ded5a54963dbdb4b273799e1401fa4b7b067046
Chris Meeder <cmeeder1207@gmail.com>
2018-08-05 11:57:22 -0700

    basic framework is done

---
+++ mian.js
@@ -0,0 +1,28 @@
+var Goal =Math.floor(Math.random(19-3)*100 + 2)
+
+var user = {}
+
+var wins=0
+var losses=0
+
+var cry1= Math.floor(Math.random(19-3)*10 + 2)
+
+var cry2=Math.floor(Math.random(19-3)*10 + 2)
+
+var cry3=Math.floor(Math.random(19-3)*10 + 2)
+
+var cry4=Math.floor(Math.random(19-3)*10 + 2)
+
+
+
+
+//-------------------- FOR TESTING ONLY-----------------------//
+console.log("cry 1", cry1)
+
+console.log("cry 2", cry2)
+
+console.log("cry 3", cry3)
+
+console.log("cry 4", cry4)
+
+console.log("goal",Goal)
